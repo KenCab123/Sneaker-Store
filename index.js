fd8160a84b6319c2e58ce0b8b51f30c9d7930c0c
KenCab123 <KennethCabral@pursuit.org>
2023-10-17 12:57:52 -0400

    created run function that does an action depending on the word you type into the terminal. All the functions working properly

--- index.js
+++ index.js
@@ -4,3 +4,43 @@ const { create, index, show, destroy, edit} = require('./src/controller');
 const inform = console.log
 
 const sneakers = readJSONFile(`./data`, `sneakerCart.json`);
+
+const run = () => {
+    let writeToFile = false;
+    let updatedSneaker = [];
+
+    const action = process.argv[2];
+    const sneaker = process.argv[3];
+
+    switch(action) {
+        case 'create':
+            updatedSneaker = create(sneakers, sneaker);
+            writeToFile = true
+            break;
+        case 'index':
+            const sneakersView = index(sneakers);
+            inform(sneakersView);
+            break;
+        case 'show':
+            const sneakerView = show(sneakers, sneaker);
+            inform(sneakerView);
+            break;
+        case 'destroy':
+            updatedSneaker = destroy(sneakers, sneaker);
+            writeToFile = true;
+            break;
+        case 'update':
+            updatedSneaker = edit(sneakers, sneaker, process.argv[4]);
+            writeToFile = true;
+            break;
+        default:
+            inform(`There was an error.`);
+    }
+
+
+    if(writeToFile) {
+        writeJSONFile(`./data`, `sneakerCart.json`, updatedSneaker)
+    }
+}
+
+run()
